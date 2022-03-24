7c2ca9e8d2f722caaec1cef69a19410a0b241411
qinzuoyan <qinzuoyan@xiaomi.com>
2022-03-24 23:39:51 +0800

    update files

--- leetcode-cn/42-2.cpp
+++ leetcode-cn/42-2.cpp
@@ -1,43 +1,24 @@
+#include <stack>
 #include <vector>
+#include <utility>
 using namespace std;
 
 class Solution {
 public:
     // O(n)
     int trap(vector<int>& height) {
-        int max_pos = 0, n = height.size();
-        for (int i = 1; i < n; i++) {
-            if (height[i] > height[max_pos])
-                max_pos = i;
-        }
-        int sum = 0;
-        if (max_pos > 1) {
-            int last_pos = 0, i = 1, s = 0;
-            while (i <= max_pos) {
-                if (height[i] >= height[last_pos]) {
-                    sum += height[last_pos] * (i - last_pos - 1) - s;
-                    last_pos = i;
-                    s = 0;
+        int n = height.size(), sum = 0;
+        stack<int> s;
+        for (int i = 0; i < n; i++) {
+            int h = height[i];
+            while (!s.empty() && h > height[s.top()]) {
+                int left = height[s.top()];
+                s.pop();
+                if (!s.empty()) {
+                    sum += (min(height[s.top()], h) - left) * (i - s.top() - 1);
                 }
-                else {
-                    s += height[i];
-                }
-                i++;
             }
-        }
-        if (max_pos < n - 2) {
-            int last_pos = n - 1, i = n - 2, s = 0;
-            while (i >= max_pos) {
-                if (height[i] >= height[last_pos]) {
-                    sum += height[last_pos] * (last_pos - i - 1) - s;
-                    last_pos = i;
-                    s = 0;
-                }
-                else {
-                    s += height[i];
-                }
-                i--;
-            }
+            s.push(i);
         }
         return sum;
     }
--- leetcode-cn/739-2.cpp
+++ leetcode-cn/739-2.cpp
@@ -8,14 +8,14 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
         vector<int> r(n);
-        stack<pair<int,int>> s;
+        stack<int> s;
         for (int i = 0; i < n; i++) {
             int t = temperatures[i];
-            while (!s.empty() && s.top().first < t) {
-                r[s.top().second] = i - s.top().second;
+            while (!s.empty() && temperatures[s.top()] < t) {
+                r[s.top()] = i - s.top();
                 s.pop();
             }
-            s.emplace(t, i);
+            s.push(i);
         }
         return r;
     }
