5700a7e0d9462ae9a27e61cc20b8ce84e1dee967
qinzuoyan <qinzuoyan@xiaomi.com>
2022-03-26 22:30:31 +0800

    add files

---
+++ leetcode-cn/38.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <cstdio>
+using namespace std;
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        string pre = "1", cur;
+        int i = 2;
+        char buf[100];
+        while (i <= n) {
+            cur.clear();
+            string cur;
+            int m = pre.size();
+            int c = 1;
+            for (int j = 1; j < m; j++) {
+                if (pre[j] != pre[j-1]) {
+                    sprintf(buf, "%d", c);
+                    cur += buf;
+                    cur += pre[j-1];
+                    c = 1;
+                }
+                else {
+                    c++;
+                }
+            }
+            sprintf(buf, "%d", c);
+            cur += buf;
+            cur += pre[m-1];
+            pre.swap(cur);
+            i++;
+        }
+        return pre;
+    }
+};
