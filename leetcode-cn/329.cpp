02bda7e215e741980fc4131b6f6e4eeb2f6146a8
qinzuoyan <qinzuoyan@xiaomi.com>
2022-03-30 10:27:28 +0800

    add files

---
+++ leetcode-cn/329.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<vector<int>>& matrix, vector<vector<int>>& longest,
+            size_t i, size_t j) {
+        if (longest[i][j] > 0)
+            return longest[i][j];
+        int m = 0, c = matrix[i][j];
+        if (i > 0 && matrix[i-1][j] > c)
+            m = max(m, search(matrix, longest, i-1, j));
+        if (i < matrix.size()-1 && matrix[i+1][j] > c)
+            m = max(m, search(matrix, longest, i+1, j));
+        if (j > 0 && matrix[i][j-1] > c)
+            m = max(m, search(matrix, longest, i, j-1));
+        if (j < matrix[0].size()-1 && matrix[i][j+1] > c)
+            m = max(m, search(matrix, longest, i, j+1));
+        longest[i][j] = m + 1;
+        return longest[i][j];
+    }
+
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        size_t m = matrix.size();
+        size_t n = matrix[0].size();
+        int max = 0;
+        vector<vector<int>> longest(m, vector<int>(n));
+        for (size_t i = 0; i < m; i++) {
+            for (size_t j = 0; j < n; j++) {
+                int r = search(matrix, longest, i, j);
+                if (r > max) {
+                    max = r;
+                }
+            }
+        }
+        return max;
+    }
+};
--- leetcode-cn/79.cpp
+++ leetcode-cn/79.cpp
@@ -4,34 +4,34 @@ using namespace std;
 
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, vector<vector<bool>>& visit, size_t i, size_t j,
+    bool exist(vector<vector<char>>& board, size_t i, size_t j,
             const string& word, size_t pos) {
-        if (visit[i][j] || board[i][j] != word[pos])
+        if (board[i][j] < 0 || board[i][j] != word[pos])
             return false;
-        visit[i][j] = true;
+        board[i][j] = -board[i][j];
         if (pos == word.size() - 1)
             return true;
-        if (i > 0 && exist(board, visit, i-1, j, word, pos+1))
+        if (i > 0 && exist(board, i-1, j, word, pos+1))
             return true;
-        if (i < board.size()-1 && exist(board, visit, i+1, j, word, pos+1))
+        if (i < board.size()-1 && exist(board, i+1, j, word, pos+1))
             return true;
-        if (j > 0 && exist(board, visit, i, j-1, word, pos+1))
+        if (j > 0 && exist(board, i, j-1, word, pos+1))
             return true;
-        if (j < board[0].size()-1 && exist(board, visit, i, j+1, word, pos+1))
+        if (j < board[0].size()-1 && exist(board, i, j+1, word, pos+1))
             return true;
-        visit[i][j] = false;
+        board[i][j] = -board[i][j];
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
         size_t m = board.size();
         size_t n = board[0].size();
-        vector<vector<bool>> visit(m);;
-        for (vector<bool>& v : visit)
-            v.resize(n);
-        for (size_t i = 0; i < m; i++)
-            for (size_t j = 0; j < n; j++)
-                if (exist(board, visit, i, j, word, 0))
+        for (size_t i = 0; i < m; i++) {
+            for (size_t j = 0; j < n; j++) {
+                if (exist(board, i, j, word, 0)) {
                     return true;
+                }
+            }
+        }
         return false;
     }
 };
