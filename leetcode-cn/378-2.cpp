7bf0a9d04bb3b5ff15367dc1cc9cfa2923dcb45c
qinzuoyan <qinzuoyan@xiaomi.com>
2022-04-02 13:45:04 +0800

    add files

---
+++ leetcode-cn/378-2.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int m = matrix.size(), n = matrix[0].size();
+        priority_queue<int> q;
+        for (auto& v : matrix) {
+            for (int i : v) {
+                if ((int)q.size() < k) {
+                    q.push(i);
+                }
+                else if (i < q.top()) {
+                    q.push(i);
+                    q.pop();
+                }
+            }
+        }
+        return q.top();
+    }
+};
---
+++ leetcode-cn/954-2.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <cstring>
+using namespace std;
+
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& arr) {
+        if (arr.empty()) {
+            return true;
+        }
+        int a[2][100001];
+        memset(a, 0, sizeof(a));
+        int c0 = 0, a0 = 0, a1 = 0;
+        for (int i : arr) {
+            if (i == 0) {
+                c0++;
+            }
+            else if (i > 0) {
+                a[0][i]++;
+                a0++;
+            }
+            else {
+                a[1][-i]++;
+                a1++;
+            }
+        }
+        if ((c0 & 1) || (a0 & 1) || (a1 & 1)) {
+            return false;
+        }
+        for (int i = 0; i < 2; i++) {
+            int* m = a[i];
+            for (int j = 1; j <= 100000; j++) {
+                if (m[j] > 0) {
+                    if ((m[j << 1] -= m[j]) < 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
