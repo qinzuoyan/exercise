c58237f61be3221f6ab54b853c68262c895cdbfa
qinzuoyan <qinzuoyan@xiaomi.com>
2022-04-09 23:10:53 +0800

    add files

---
+++ leetcode-cn/1114.cpp
@@ -0,0 +1,48 @@
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+using namespace std;
+
+class Foo {
+private:
+  mutex _mtx;
+  condition_variable _cv2;
+  condition_variable _cv3;
+  bool _ready2;
+  bool _ready3;
+
+public:
+  Foo() : _ready2(false), _ready3(false) {}
+
+  void first(function<void()> printFirst) {
+    // printFirst() outputs "first". Do not change or remove this line.
+    printFirst();
+
+    unique_lock<std::mutex> lck(_mtx);
+    _ready2 = true;
+    _cv2.notify_all();
+  }
+
+  void second(function<void()> printSecond) {
+    std::unique_lock<std::mutex> lck(_mtx);
+    while (!_ready2) {
+      _cv2.wait(lck);
+    }
+
+    // printSecond() outputs "second". Do not change or remove this line.
+    printSecond();
+
+    _ready3 = true;
+    _cv3.notify_all();
+  }
+
+  void third(function<void()> printThird) {
+    std::unique_lock<std::mutex> lck(_mtx);
+    while (!_ready3) {
+      _cv3.wait(lck);
+    }
+
+    // printThird() outputs "third". Do not change or remove this line.
+    printThird();
+  }
+};
---
+++ leetcode-cn/1115.cpp
@@ -0,0 +1,48 @@
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+using namespace std;
+
+class FooBar {
+private:
+  int n;
+  mutex mtx;
+  condition_variable cv;
+  int c;
+
+public:
+  FooBar(int n) {
+    this->n = n;
+    this->c = 0;
+  }
+
+  void foo(function<void()> printFoo) {
+
+    for (int i = 0; i < n; i++) {
+      unique_lock<mutex> lck(mtx);
+      while (c != 0)
+        cv.wait(lck);
+
+      // printFoo() outputs "foo". Do not change or remove this line.
+      printFoo();
+
+      c = 1;
+      cv.notify_all();
+    }
+  }
+
+  void bar(function<void()> printBar) {
+
+    for (int i = 0; i < n; i++) {
+      unique_lock<mutex> lck(mtx);
+      while (c != 1)
+        cv.wait(lck);
+
+      // printBar() outputs "bar". Do not change or remove this line.
+      printBar();
+
+      c = 0;
+      cv.notify_all();
+    }
+  }
+};
