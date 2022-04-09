f57da58aa0caa8abc65e10cb6e45ba1fd57ed775
qinzuoyan <qinzuoyan@xiaomi.com>
2022-04-09 22:16:35 +0800

    add files

--- leetcode-cn/310.cpp
+++ leetcode-cn/310.cpp
@@ -4,74 +4,73 @@ using namespace std;
 
 class Solution {
  public:
-  static const int kNoVisit = 0;
-  static const int kMostVisit = 1;
-  static const int kAllVisit = 2;
+  static const int kNoVisit = -1;
+  static const int kAllVisit = -2;
   struct Stat {
-    int stat;               // kNoVisit || kMostVisit || kAllVisit
-    pair<int, int> max[2];  // <nodeid, height>
-    int unvisit;            // used only when stat == kMostVisit
-    Stat() : stat(kNoVisit), unvisit(-1) { max[0].first = max[1].first = -1; }
+    int stat;               // kNoVisit || kAllVisit || kMostVisit(>=0)
+    int max0, max1;
+    int max0n;
+    Stat() : stat(kNoVisit), max0(0), max1(0), max0n(0) {}
   };
 
-  int findHeightExcept(vector<Stat>& stats, vector<vector<int>>& n2n, int root,
+  int findHeightExcept(Stat* stats, vector<int>* n2n, int root,
                        int except) {
     Stat& s = stats[root];
-    if (s.stat == kNoVisit) {
+    if (s.stat == kNoVisit) { // kNoVisit
       vector<int>& v = n2n[root];
       for (int i : v) {
         if (i != except) {
           int h = findHeightExcept(stats, n2n, i, root) + 1;
-          if (h > s.max[0].second) {
-            s.max[1] = s.max[0];
-            s.max[0] = pair<int, int>(i, h);
-          } else if (h > s.max[1].second) {
-            s.max[1] = pair<int, int>(i, h);
+          if (h > s.max0) {
+            s.max1 = s.max0;
+            s.max0 = h;
+            s.max0n = i;
+          }
+          else if ( h > s.max1) {
+            s.max1 = h;
           }
         }
-        if (except >= 0) {
-          s.stat = kMostVisit;
-          s.unvisit = except;
-        } else {
-          s.stat = kAllVisit;
-        }
+        s.stat = except >= 0 ? except : kAllVisit;
       }
-      return s.max[0].second;
-    } else if (s.stat == kMostVisit) {
-      if (except == s.unvisit) {
-        return s.max[0].second;
+      return s.max0;
+    } else if (s.stat >= 0) { // kMostVisit
+      if (s.stat == except) {
+        return s.max0;
       } else {
-        int h = findHeightExcept(stats, n2n, s.unvisit, root) + 1;
-        if (h > s.max[0].second) {
-          s.max[1] = s.max[0];
-          s.max[0] = pair<int, int>(s.unvisit, h);
-        } else if (h > s.max[1].second) {
-          s.max[1] = pair<int, int>(s.unvisit, h);
+        int h = findHeightExcept(stats, n2n, s.stat, root) + 1;
+        if (h > s.max0) {
+          s.max1 = s.max0;
+          s.max0 = h;
+          s.max0n = s.stat;
+        } else if (h > s.max1) {
+          s.max1 = h;
         }
         s.stat = kAllVisit;
-        return s.max[0].first != except ? s.max[0].second : s.max[1].second;
+        return s.max0n == except ? s.max1 : s.max0;
       }
-    } else {  // kAllVisit
-      return s.max[0].first != except ? s.max[0].second : s.max[1].second;
+    } else { // kAllVisit
+      return s.max0n == except ? s.max1 : s.max0;
     }
   }
 
   vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-    vector<vector<int>> n2n(n);
+    vector<int> n2n[n];
     for (auto& v : edges) {
       n2n[v[0]].push_back(v[1]);
       n2n[v[1]].push_back(v[0]);
     }
-    vector<Stat> stats(n);
+    Stat stats[n];
+    int h[n];
     int r = INT_MAX;
+    for (int i = 0; i < n; i++) {
+      h[i] = findHeightExcept(stats, n2n, i, -1);
+      if (h[i] < r) {
+        r = h[i];
+      }
+    }
     vector<int> v;
     for (int i = 0; i < n; i++) {
-      int h = findHeightExcept(stats, n2n, i, -1);
-      if (h == r) {
-        v.push_back(i);
-      } else if (h < r) {
-        r = h;
-        v.clear();
+      if (h[i] == r) {
         v.push_back(i);
       }
     }
