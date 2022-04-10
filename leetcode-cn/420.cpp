40c0b1f63512b112a467c71566e0963bd52a9fb3
qinzuoyan <qinzuoyan@xiaomi.com>
2022-04-10 19:53:21 +0800

    add 420

--- leetcode-cn/420.cpp
+++ leetcode-cn/420.cpp
@@ -1,41 +1,86 @@
 #include <string>
+#include <map>
+#include <cctype>
 using namespace std;
 
 class Solution {
-  struct Del {
-    int pos;
-    int len;
-    int edit;
-    Del(int p, int l, int e) : pos(p), len(l), edit(e) {}
-  };
 public:
   int strongPasswordChecker(string password) {
-    cout << password << endl;
     int n = password.size();
-    int k = 0, i = 1;
-    vector<int> v;
-    vector<Del> v;
+    multimap<int, int> m; // edit : len
+    int len = 0, k = 0, b = 0, i = 0;
     for (; i < n; i++) {
-      if (password[i] != password[i-1]) {
-        if (i - k >= 3) {
-          v.emplace_back(k, i - k, (i - k - 2) % 3);
-          if (v.back().edit == 0) {
-            v.back().edit = 3;
-          }
+      if (b != 7) {
+        if (isupper(password[i]))
+          b |= 4;
+        else if (islower(password[i]))
+          b |= 2;
+        else if (isdigit(password[i]))
+          b |= 1;
+      }
+      if (i > 0 && password[i] != password[i-1]) {
+        len = i - k;
+        if (len >= 3) {
+          if (n > 20)
+            m.emplace(len % 3 + 1, len);
+          else
+            m.emplace((len + 1) % 3 + 1, len);
         }
         k = i;
       }
     }
-    if (i - k >= 3) {
-      v.emplace_back(k, i - k, (i - k - 2) % 3);
-      if (v.back().edit == 0) {
-        v.back().edit = 3;
+    len = i - k;
+    if (len >= 3) {
+      if (n > 20)
+        m.emplace(len % 3 + 1, len);
+      else
+        m.emplace((len + 1) % 3 + 1, len);
+    }
+    int r = 0, an = (b >> 2) + ((b & 2) >> 1) + (b & 1);
+    if (n > 20) {
+      // delete
+      while (n > 20 && m.size()) {
+        len = m.begin()->second;
+        m.erase(m.begin());
+        len--;
+        if (len >= 3) {
+          m.emplace(len % 3 + 1, len);
+        }
+        r++;
+        n--;
+      }
+      if (n > 20) {
+        r += n - 20;
       }
     }
-    if (
-    for (int i : v) {
-      cout << i << "," << endl;
+    else if (n < 6) {
+      // insert
+      while (n < 6 && m.size()) {
+        len = m.begin()->second;
+        m.erase(m.begin());
+        len -= 2;
+        if (len >= 3) {
+          m.emplace((len + 1) % 3 + 1, len);
+        }
+        an++;
+        r++;
+        n++;
+      }
+      if (n < 6) {
+        int e = 6 - n;
+        an += e;
+        r += e;
+      }
     }
-    return 0;
+    // modify
+    for (auto& pr : m) {
+      int e = pr.second / 3;
+      an += e;
+      r += e;
+    }
+    if (an < 3) {
+      r += 3 - an;
+    }
+    return r;
   }
 };
