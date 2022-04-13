af0ca4cd18f995d1fa4ac5c69379ea553e9e67dd
qinzuoyan <qinzuoyan@xiaomi.com>
2022-04-13 20:25:28 +0800

    add files

---
+++ leetcode-cn/LCP05.cpp
@@ -0,0 +1,57 @@
+#include <vector>
+using namespace std;
+
+// Time out ...
+class Solution {
+ public:
+  const static long long M = 1000000007;
+  struct N {
+    int parent;
+    int members;    // subtree member count
+    int coins;      // coins to all subtree
+    int coins_sum;  // subtree coins sum
+    N() : parent(0), members(1), coins(0), coins_sum(0) {}
+  };
+  vector<int> bonus(int n, vector<vector<int>>& leadership,
+                    vector<vector<int>>& operations) {
+    N a[n + 1];
+    for (auto& v : leadership) {
+      int parent = v[0], child = v[1];
+      a[child].parent = parent;
+      while (parent) {
+        a[parent].members += a[child].members;
+        parent = a[parent].parent;
+      }
+    }
+    vector<int> r;
+    for (auto& v : operations) {
+      if (v[0] == 1) { // give one
+        int i = v[1], c = v[2];
+        while (i) {
+          a[i].coins_sum += c;
+          i = a[i].parent;
+        }
+      }
+      else if (v[0] == 2) { // give all
+        int i = v[1], c = v[2];
+        int k = a[i].members * c;
+        a[i].coins += c;
+        while (i) {
+          a[i].coins_sum += k;
+          i = a[i].parent;
+        }
+      }
+      else { // query all
+        int i = v[1];
+        long long c = a[i].coins_sum, m = a[i].members;
+        i = a[i].parent;
+        while (i) {
+          c += a[i].coins * m;
+          i = a[i].parent;
+        }
+        r.push_back(c % M);
+      }
+    }
+    return r;
+  }
+};
--- leetcode-cn/test.cpp
+++ leetcode-cn/test.cpp
@@ -6,10 +6,17 @@
 #include <fstream>
 #include <queue>
 using namespace std;
+#include "LCP05.cpp"
 
 int main() {
-  // Solution s;
+  Solution s;
+  int n = 6;
+  vector<vector<int>> leadership = { {1, 2}, {1, 6}, {2, 3}, {2, 5}, {1, 4} };
+  vector<vector<int>> operations = { {1, 1, 500}, {2, 2, 50}, {3, 1}, {2, 6, 15}, {3, 1} };
+  vector<int> r = s.bonus(n, leadership, operations);
+  for (int i : r) {
+    cout << i << "," << endl;
+  }
   return 0;
 }
 
-/* vim: set ts=4 sw=4 sts=4 tw=100 */
