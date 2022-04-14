e38edb8ca0c1abd72565da53281235d368550f40
qinzuoyan <qinzuoyan@xiaomi.com>
2022-04-14 09:59:39 +0800

    add files

---
+++ leetcode-cn/1672.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int maximumWealth(vector<vector<int>>& accounts) {
+    int m = 0;
+    for (auto& v : accounts) {
+      int n = 0;
+      for (int i : v) n += i;
+      if (n > m) m = n;
+    }
+    return m;
+  }
+};
