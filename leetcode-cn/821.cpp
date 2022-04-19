e77b313ea516c1aeb87d9cffbd820c680b55131b
qinzuoyan <qinzuoyan@xiaomi.com>
2022-04-19 14:05:01 +0800

    add files

---
+++ leetcode-cn/821.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> shortestToChar(string s, char c) {
+    int n = s.size(), h = -1, t, m, k = 0;
+    vector<int> v(n);
+    while (++h < n && s[h] != c);
+    do {
+      t = h;
+      while (++h < n && s[h] != c);
+      m = (h + t) / 2;
+      while (k <= m) v[k++] = (k <= t ? t - k : k - t);
+    } while (h < n);
+    while (k < n) v[k++] = (k <= t ? t - k : k - t);
+    return v;
+  }
+};
