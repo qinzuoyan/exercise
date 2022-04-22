63f3bf71e4ca5b00ad064ae4c12faeee1123221b
qinzuoyan <qinzuoyan@xiaomi.com>
2022-04-22 14:44:19 +0800

    add files

---
+++ leetcode-cn/396.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int maxRotateFunction(vector<int>& nums) {
+    int s = 0, x = 0, n = nums.size();
+    for (int i = 0; i < n; i++) {
+      s += nums[i];
+      x += i * nums[i];
+    }
+    int m = x;
+    for (int i = 0; i < n - 1; i++) {
+      x -= s - nums[i] * n;
+      if (x > m) m = x;
+    }
+    return m;
+  }
+};
