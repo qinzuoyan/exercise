a819f67ea2bb6f3dd370daad6a4d2f1251cc3d18
qinzuoyan <qinzuoyan@xiaomi.com>
2022-05-04 14:46:14 +0800

    add files

---
+++ leetcode-cn/1823.cpp
@@ -0,0 +1,21 @@
+class Solution {
+ public:
+  int findTheWinner(int n, int k) {
+    int a[n], h = 0, s = 0;
+    for (int i = 0; i < n; ++i) a[i] = i + 1;
+    while (n > 1) {
+      int s = (k + s - 1) % n;
+      if (s == 0) {
+        h = a[h];
+      } else if (s == n - 1) {
+        s = 0;
+      } else {
+        int p = h, t = s;
+        while (--t > 0) p = a[p];
+        a[p] = a[a[p]];
+      }
+      --n;
+    }
+    return h + 1;
+  }
+};
