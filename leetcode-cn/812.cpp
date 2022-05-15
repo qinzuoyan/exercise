d9710d7a9b7c8c133133d73c6918176041dba5e0
qinzuoyan <qinzuoyan@xiaomi.com>
2022-05-15 20:10:54 +0800

    add files

---
+++ leetcode-cn/812.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  double largestTriangleArea(vector<vector<int>>& points) {
+    int n = points.size();
+    double max = 0;
+    for (int i = 0; i < n - 2; ++i) {
+      for (int j = i + 1; j < n - 1; ++j) {
+        for (int k = j + 1; j < n; ++k) {
+          vector<int>&p0 = points[i], &p1 = points[j], &p2 = points[k];
+          double s = (double)((p1[0] - p0[0]) * (p2[1] - p0[1]) -
+                              (p2[0] - p0[0]) * (p1[1] - p0[1])) /
+                     2;
+          if (s < 0) s = -s;
+          if (s > max) max = s;
+        }
+      }
+    }
+    return max;
+  }
+};
