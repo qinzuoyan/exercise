7769a62d8f949639ac9c8c8937555313ce7a41db
qinzuoyan <qinzuoyan@xiaomi.com>
2022-05-17 08:19:47 +0800

    add files

---
+++ leetcode-cn/0406.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+ public:
+  TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+    if (root == nullptr || p == nullptr) return nullptr;
+    if (p->right) {
+      p = p->right;
+      while (p && p->left) p = p->left;
+      return p;
+    } else {
+      TreeNode *q = root, *r = nullptr;
+      while (q->val != p->val) {
+        if (q->val > p->val) {
+          r = q;
+          q = q->left;
+        } else {
+          q = q->right;
+        }
+      }
+      return r;
+    }
+  }
+};
---
+++ leetcode-cn/953.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  bool isAlienSorted(vector<string>& words, string order) {
+    if (words.size() == 1) return true;
+    int n = words.size();
+    int a[26];
+    for (int i = 0; i < 26; ++i) {
+      a[order[i] - 'a'] = i + 1;
+    }
+    for (int i = 1; i < n; ++i) {
+      const char *p1 = words[i - 1].data(), *p2 = words[i].data();
+      while (*p1 && *p1 == *p2) ++p1, ++p2;
+      if (*p1 && (*p2 == '\0' || a[*p1 - 'a'] > a[*p2 - 'a'])) return false;
+    }
+    return true;
+  }
+};
