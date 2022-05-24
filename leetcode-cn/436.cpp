1e734d60969a63e9eee201da6f8abebc76d476c9
qinzuoyan <qinzuoyan@xiaomi.com>
2022-05-24 12:23:10 +0800

    add files

---
+++ leetcode-cn/436.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <map>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> findRightInterval(vector<vector<int>>& intervals) {
+    int n = intervals.size();
+    map<int, int> m;
+    for (int i = 0; i < n; ++i) {
+      m[intervals[i][0]] = i;
+    }
+    vector<int> r(n);
+    for (int i = 0; i < n; ++i) {
+      auto it = m.lower_bound(intervals[i][1]);
+      r[i] = (it == m.end() ? -1 : it->second);
+    }
+    return r;
+  }
+};
