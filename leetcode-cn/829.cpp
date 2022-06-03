642e00a61ede4508990939930adaab5f84c26592
qinzuoyan <qinzuoyan@xiaomi.com>
2022-06-04 00:13:29 +0800

    add files

---
+++ leetcode-cn/829.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  int consecutiveNumbersSum(int n) {
+    int r = 1;
+    for (int i = 2; n / i >= (i + 1) / 2; ++i) {
+      if (i & 1) {
+        if (n % i == 0)
+          r++;
+      } else {
+        if (n % i == (i >> 1))
+          r++;
+      }
+    }
+    return r;
+  }
+};
