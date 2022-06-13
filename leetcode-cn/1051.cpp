fe38cd3fe88b027da04dadbf461164f368d6164d
qinzuoyan <qinzuoyan@xiaomi.com>
2022-06-13 09:57:00 +0800

    add files

---
+++ leetcode-cn/1051.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+ public:
+  int heightChecker(vector<int>& heights) {
+    vector<int> a(heights);
+    sort(a.begin(), a.end());
+    int r = 0, n = a.size();
+    for (int i = 0; i < n; ++i) {
+      if (a[i] != heights[i]) ++r;
+    }
+    return r;
+  }
+};
