f49b3bd8cdaf490eb00cf777b84234adf1c7cc82
qinzuoyan <qinzuoyan@xiaomi.com>
2023-02-01 21:33:39 +0800

    add files

---
+++ leetcode-cn/2325.cpp
@@ -0,0 +1,24 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+ string decodeMessage(string key, string message) {
+   char a[26];
+   memset(a, 0, sizeof(a));
+   char k = 'a';
+   for (char c : key) {
+     if (c != ' ' && a[c - 'a'] == 0) {
+       a[c - 'a'] = k++;
+     }
+   }
+   int n = message.size();
+   for (int i = 0; i < n; ++i) {
+     char& c = message[i];
+     if (c != ' ') {
+       c = a[c - 'a'];
+     }
+   }
+   return message;
+ }
+};
